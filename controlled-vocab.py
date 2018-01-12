c2481bc22e6b2d36ebacffc1251c3415eacf27e0
Eric Cornelissen <ericornelissen@gmail.com>
2018-01-12 11:22:48 +0100

    Multiple input files and persere input order
    Update the tool such that it can now process multiple files at once
    (using the same mapping or all files) and also perserve the order of the
    input file. It is also possible to merge the results of the multiple
    input files into a single file. It is now also possible to export the
    mapping generated.
    
    Also added a third example input file with 1048576 lines of data.

--- controlled-vocab.py
+++ controlled-vocab.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import threading
 import time
@@ -13,19 +14,22 @@ def show_help():
     print('    - If you don\'t type a value and press ENTER, the output value will be the\n      same as the input value.')
     print('')
     print('Arguments:')
-    print('    -c, --case-sensitive     Should the conversion be case sensitive (defaults is False)')
-    print('    -f, --file               List of input files')
-    print('    -h, --help               Shows this help message')
-    print('    -o, --output             File to write the result to')
+    print('    -c, --case-sensitive     Should the conversion be case sensitive (defaults\n                             is False).')
+    print('    -i, --input              List of input files.')
+    print('    -h, --help               Shows this help message.')
+    print('    -m, --merge              Merge to results into a single file.')
+    print('    -o, --output             List of output files for each input file, followed\n                             by a file to export the mapping (JSON format).')
 
 
 # Configuration
 def parse_args(args):
     """Parse the configuration arguments"""
     config = {
-        'input': [],
-        'output': None,
-        'case_sensitive': False
+        'case_sensitive': False,
+        'input_files':    [],
+        'mapping_file':   None,
+        'merge':          False,
+        'output_files':   []
     }
 
     current_key = None
@@ -33,22 +37,28 @@ def parse_args(args):
         if arg[0:1] == '-':
             if arg == '--case-sensitive' or arg == '-c':
                 current_key = 'case_sensitive'
-            elif arg == '--file' or arg == '-f':
-                current_key = 'input'
+                config['case_sensitive'] = True
             elif arg == '--help' or arg == '-h':
                 show_help()
                 exit(0)
+            elif arg == '--input' or arg == '-i':
+                current_key = 'input'
+            elif arg == '--merge' or arg == '-m':
+                current_key = 'merge'
+                config['merge'] = True
             elif arg == '--output' or arg == '-o':
                 current_key = 'output'
             else:
+                print(f'Unknown argument "{arg}"...')
                 current_key = None
         else:
-            if current_key == 'case_sensitive':
-                config['case_sensitive'] = True if arg.lower() == 'true' else False
-            elif current_key == 'input':
-                config['input'].append(arg)
+            if current_key == 'input':
+                config['input_files'].append(arg)
             elif current_key == 'output':
-                config['output'] = arg
+                if len(config['output_files']) < len(config['input_files']):
+                    config['output_files'].append(arg)
+                else:
+                    config['mapping_file'] = arg
 
     return config
 
@@ -73,27 +83,36 @@ finished_converting = False
 
 # Thread classes
 class ReadThread(threading.Thread):
-    def __init__(self, files):
+    def __init__(self, input_files, output_files, merge):
         super(ReadThread, self).__init__()
-        global finished_reading
 
-        if len(files) == 0:
-            finished_reading = True # Stops the ConvertThread
-            raise RuntimeError('no input')
+        if len(input_files) == 0:
+            show_help()
+            print('\nNo input files found...\n')
+            exit(0)
 
-        self.files = files
+        self.input_files = input_files
         self.local_values = []
+        self.merge = merge
+        self.output_files = output_files
 
     ### Thread
     def run(self):
         global finished_reading
+        total_line_counter = 0
 
         # Read the content of each input file
-        for file_path in self.files:
+        for i, file_path in enumerate(self.input_files):
             file = open(file_path, 'r')
-            for line in file:
-                value = line[:-1]
-                self.local_values.append(value)
+            for number, value in enumerate(file):
+                line = {
+                    'file': self.output_files[i] if len(self.output_files) > i else None,
+                    'line_nr': number if self.merge is False else total_line_counter,
+                    'value': value[:-1]
+                }
+
+                self.local_values.append(line)
+                total_line_counter += 1
 
                 # Publish the local values every 50000 lines
                 if len(self.local_values) > 50000:
@@ -142,30 +161,32 @@ class ConvertThread(threading.Thread):
                 i = len(self.local_waiting)
                 while i > 0:
                     i -= 1
-                    value = self.local_waiting[i]
-                    if self.convert_value(value, prompt=False):
+                    waiting = self.local_waiting[i]
+                    if self.convert_value(waiting, prompt=False):
                         self.local_waiting.pop(i)
 
-            for value in self.local_input:
-                self.convert_value(value, prompt=True)
+            for input in self.local_input:
+                self.convert_value(input, prompt=True)
 
             self.publish()
 
         finished_converting = True
 
     ### ConvertThread
-    def convert_value(self, value, prompt):
+    def convert_value(self, input, prompt):
         """Convert a value into another value based on the known (local) mapping"""
+        value = input['value']
         key = value
         if self.case_sensitive is False:
             key = value.lower()
 
         # If the key is mapped add to result, otherwise prompt user
         if key in self.local_mapping:
-            self.local_output.append(self.local_mapping[key])
+            input['value'] = self.local_mapping[key]
+            self.local_output.append(input)
             return True
         elif prompt is True:
-            self.prompt(value)
+            self.prompt(input)
 
         return False
 
@@ -178,10 +199,11 @@ class ConvertThread(threading.Thread):
         global_input = [] # Clear published values as to not process them twice
         input_semaphore.release()
 
-    def prompt(self, value):
+    def prompt(self, input):
         """Promp the user to give a mapping"""
         global prompt_semaphore, global_prompt
 
+        value = input['value']
         key = value
         if self.case_sensitive is False:
             key = value.lower()
@@ -191,17 +213,18 @@ class ConvertThread(threading.Thread):
             global_prompt.append(value)
         prompt_semaphore.release()
 
-        self.local_waiting.append(value)
+        self.local_waiting.append(input)
 
     def publish(self):
         """Publish the converted values to the global output"""
         global output_semaphore, global_output
 
-        output_semaphore.acquire()
-        global_output = global_output + self.local_output
-        output_semaphore.release()
+        if len(self.local_output) > 0:
+            output_semaphore.acquire()
+            global_output = global_output + self.local_output
+            output_semaphore.release()
 
-        self.local_output = []
+            self.local_output = []
 
     def update_mapping(self):
         """Synchronize the local mapping with the global mapping"""
@@ -269,11 +292,18 @@ class PromptThread(threading.Thread):
         mapping_semaphore.release()
 
 class WriteThread(threading.Thread):
-    def __init__(self, file):
+    def __init__(self, output_files, mapping_file, merge):
         super(WriteThread, self).__init__()
 
-        self.file = open(file, 'w+') if file is not None else None
+        self.current_ln = {} if output_files is not None else None
         self.local_values = []
+        self.mapping_file = open(mapping_file, 'w+') if mapping_file is not None else None
+        self.output_file = output_files[0] if merge is True else None
+        self.output_files = {} if output_files is not None else None
+
+        for file in output_files:
+            self.output_files[file] = open(file, 'w+')
+            self.current_ln[file] = 0
 
     ### Thread
     def run(self):
@@ -281,20 +311,24 @@ class WriteThread(threading.Thread):
         while True:
             self.get_values()
 
-            if self.file is not None:
+            if self.has_values():
                 self.write_results()
             else:
                 time.sleep(0.5)
 
-            if finished_converting:
+            if finished_converting is True and not self.has_values():
                 break
 
-        if self.file is not None:
-            self.file.close()
+        if self.output_files is not None:
+            for _, file in self.output_files.items():
+                file.close()
         else:
-            self.print_results()
+            self.print_output()
 
-    ### ConvertThread
+        if self.mapping_file is not None:
+            self.write_mapping()
+
+    ### WriteThread
     def get_values(self):
         """Read the values published to global_input"""
         global output_semaphore, global_output
@@ -304,7 +338,10 @@ class WriteThread(threading.Thread):
         global_output = [] # Clear published values as to not process them twice
         output_semaphore.release()
 
-    def print_results(self):
+    def has_values(self):
+        return len(self.local_values) > 0
+
+    def print_output(self):
         """Print the results to the command line"""
         print('')
         self.print_mapping()
@@ -312,9 +349,28 @@ class WriteThread(threading.Thread):
 
     def write_results(self):
         """Write the results to the output file"""
-        for value in self.local_values:
-            self.file.write(value + '\n')
-        self.local_values = []
+        self.local_values = sorted(self.local_values, key=lambda k: k['line_nr'], reverse=True)
+        i = len(self.local_values)
+        while i > 0:
+            i -= 1
+            line = self.local_values[i]
+
+            # Check if this is the next line in its respective file
+            file_name = line['file'] if self.output_file is None else self.output_file
+            if self.current_ln[file_name] == line['line_nr']:
+                output_file = self.output_files[file_name]
+                output_file.write(line['value'] + '\n')
+
+                # Remove the value from memory and increment the line counter
+                self.local_values.pop(i)
+                self.current_ln[file_name] += 1
+
+    def write_mapping(self):
+        """Write the mapping to the export file"""
+        if self.mapping_file is not None:
+            json_mapping = json.dumps(global_mapping)
+            self.mapping_file.write(json_mapping)
+            self.mapping_file.close()
 
     ### Utility
     def print_mapping(self):
@@ -347,7 +403,7 @@ class WriteThread(threading.Thread):
 # Main
 config = parse_args(sys.argv)
 
-reader = ReadThread(config['input'])
+reader = ReadThread(config['input_files'], config['output_files'], config['merge'])
 reader.start()
 
 converter = ConvertThread(config['case_sensitive'])
@@ -356,5 +412,5 @@ converter.start()
 prompter = PromptThread(config['case_sensitive'])
 prompter.start()
 
-writer = WriteThread(config['output'])
+writer = WriteThread(config['output_files'], config['mapping_file'], config['merge'])
 writer.start()
